1b566d3a0cd41b7c350541af583b4e4ce9cdf3fd
Bradford Simkins <SIMKINBS2614@uwec.edu>
2018-12-09 13:49:54 -0800

    Update final.r
    12/9/2018 commit. Wraping up random forest

--- final.r
+++ final.r
@@ -95,9 +95,86 @@ cor(corTestDF3)
 ######### classification of CHURN=0 or CHURN=1                                          ######### 
 ######### Bagging will have high variance because of high correlations                  ######### 
 ######### Use random forest because of correlations in predictors.  Use CV to choose P  ######### 
+######### ######### ######### ######### ######### ######### ######### ######### ######  #########
 ######### ANN best for large data sets with nonlinear relationships. Use CV to tune     #########
 ######### ######### ######### ######### ######### ######### ######### ######### ######  #########
 
 #######################################
 ###### Use random forest and ANN ######
 #######################################
+
+##################################################################################################
+#################### Random Forest for sample of 1000 obs ########################################
+##################################################################################################
+  #get a random, but representative, subset of data to work with for testing, as 90,000+ obs runs 
+  #too slowly
+set.seed(24)
+cust.sample<-cust[sample(nrow(cust),1000),]
+
+churn0<-length(which(cust$CHURN==0))
+churn0/length(cust$CHURN) #CHURN==0 represents 67% of the original data set
+
+churn0.sample<-length(which(cust.sample$CHURN==0))
+churn0.sample/length(cust.sample$CHURN) #CHURN==0 represents 66% of the sampled data set
+  #proportion of sample response variable similar to original
+
+#random forest with cross-validation for size selection
+library(randomForest)
+n<-dim(cust.sample)[1]
+k<-10
+groups<-c(rep(1:k,floor(n/k)),1:(n-floor(n/k)*k)) #list of group labels
+set.seed(22)
+cvgroups<-sample(groups,n)
+group.error<-matrix(,nr=32, nc=k) #matrix to hold error rates for each model size, in each fold
+
+#10-fold cross validation
+for (i in 1:k) { #iterate over folds
+  groupi<-(cvgroups==i)
+  for(j in 1:32){ #iterate over model size
+    cust.rf<-randomForest(cust.sample$CHURN[!groupi]~., data=cust.sample[!groupi,], mtry=j, importance=T)
+    cust.rf.pred<-predict(cust.rf,newdata=cust.sample[groupi,])
+    cust.rf.conf<-table(cust.rf.pred, cust.sample$CHURN[groupi])
+    group.error[j, i]<-(cust.rf.conf[1,2]+cust.rf.conf[2,1])/(sum(cust.rf.conf[,1])+sum(cust.rf.conf[,2]))
+  } #end iterate over model size
+} #end iterate over folds
+
+rf.mse<-apply(group.error, 1, mean)
+# [1] 0.2452472 0.2362068 0.2362276 0.2392480 0.2442478 0.2522983 0.2503187 0.2402981 0.2452882 0.2562783 0.2532783
+# [12] 0.2412682 0.2552686 0.2432583 0.2522785 0.2472583 0.2522682 0.2442583 0.2492985 0.2442781 0.2452882 0.2523187
+# [23] 0.2462882 0.2432884 0.2432682 0.2483183 0.2462985 0.2452682 0.2473084 0.2482981 0.2422680 0.2432583
+  #mtry=2,3,4 are the lowest, though no size is very different. Will go with 2 for simplicity
+
+###################### Final Random Forest Model and Error Rate, for mtry=2 ##############################
+set.seed(22)
+mtry2.rf.model<-randomForest(cust.sample$CHURN~., data=cust.sample, mtry=2, importance=T)
+cust.rf.conf<-mtry2.rf.model$confusion
+mtry2.rf.er<-(cust.rf.conf[1,2]+cust.rf.conf[2,1])/(sum(cust.rf.conf[,1])+sum(cust.rf.conf[,2])) #0.235
+
+plot(final.rf.model,xlim=c(0,150))
+  #Plot shows same error rate at 60 trees as at 500 trees. Can simplify for full model
+##########################################################################################################
+
+
+##################################################################################################
+#################### Random Forest for full data set      ########################################
+##################################################################################################
+n<-dim(cust)[1]
+k<-10
+groups<-c(rep(1:k,floor(n/k)),1:(n-floor(n/k)*k)) #list of group labels
+set.seed(22)
+cvgroups<-sample(groups,n)
+group.error<-matrix(,nr=32, nc=k) #matrix to hold error rates for each model size, in each fold
+
+#10-fold cross validation
+for (i in 1:k) { #iterate over folds
+  groupi<-(cvgroups==i)
+  for(j in 1:32){ #iterate over model size
+    cust.rf<-randomForest(cust$CHURN[!groupi]~., data=cust[!groupi,], mtry=j, importance=T)
+    cust.rf.pred<-predict(cust.rf,newdata=cust[groupi,])
+    cust.rf.conf<-table(cust.rf.pred, cust$CHURN[groupi])
+    group.error[j, i]<-(cust.rf.conf[1,2]+cust.rf.conf[2,1])/(sum(cust.rf.conf[,1])+sum(cust.rf.conf[,2]))
+  } #end iterate over model size
+} #end iterate over folds
+
+rf.mse<-apply(group.error, 1, mean)
+
